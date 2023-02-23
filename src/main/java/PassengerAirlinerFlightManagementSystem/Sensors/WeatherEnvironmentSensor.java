cf59e62f52418909237733c46ccba33a5bf37403
RileyLeo <48232039+RileyLeo@users.noreply.github.com>
2023-02-24 04:07:41 +0800

    weather sensor output logic set

--- src/main/java/PassengerAirlinerFlightManagementSystem/Sensors/WeatherEnvironmentSensor.java
+++ src/main/java/PassengerAirlinerFlightManagementSystem/Sensors/WeatherEnvironmentSensor.java
@@ -18,15 +18,42 @@ public class WeatherEnvironmentSensor implements Runnable{
         inputChannel.queueBind(queueName, FCSMain.weatherEnvironmentExchangeName, "");
 
         outputChannel = connection.createChannel();
+        outputChannel.exchangeDeclare(FCSMain.flightControlExchangeName, "fanout");
     }
-
+    boolean firstRun = true;
+    String currentWeather = "Clear";
+    String reportedWeather = "Clear";
+    boolean hasResolvedTurbulence = false;
     @Override
     public void run() {
         //send the speed to the flight control system
         try {
-            outputChannel.exchangeDeclare(FCSMain.flightControlExchangeName, "fanout");
-
-
+            // weather check
+            if (currentWeather != "Thunderstorm" || hasResolvedTurbulence == true) {
+                if (firstRun || currentWeather != reportedWeather) {
+                    String message = FCSMain.weatherEnvironmentExchangeName + ":" + currentWeather;
+                    outputChannel.basicPublish(FCSMain.flightControlExchangeName, "", null, message.getBytes("UTF-8"));
+                    System.out.println("\u001B[38;5;226m" + "Weather Environment Sensor:"+ "\u001B[0m" + currentWeather + " weather sent to flight control");
+                    reportedWeather = currentWeather;
+                    firstRun = false;
+                    System.out.println("\u001B[38;5;226m" + "Weather Environment Sensor:"+ "\u001B[0m" + "Standby for weather change");
+                }
+                //1/10 chance of weather change to rain or clear (alternating)
+                if (Math.random() < 0.1) {
+                    if (currentWeather == "Clear") {
+                        currentWeather = "Rain";
+                    } else {
+                        currentWeather = "Clear";
+                    }
+                }
+                //if it rains, 1/10 chance of thunderstorm
+                if (currentWeather == "Rain") {
+                    if (Math.random() < 0.1) {
+                        currentWeather = "Thunderstorm";
+                        hasResolvedTurbulence = false;
+                    }
+                }
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
